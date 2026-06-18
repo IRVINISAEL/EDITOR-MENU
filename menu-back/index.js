5851241a40c4a1f0aea8193d328aed26f304c259
IRVIN ISAEL MARTINEZ ALEJO <irvinmartinealejo@gmail.com>
2026-06-18 14:27:47 -0600

    Update MySQL connection environment variables

--- menu-back/index.js
+++ menu-back/index.js
@@ -14,11 +14,11 @@ app.use(express.json());
 // CONEXIÓN A MYSQL
 // ============================
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -244,7 +244,7 @@ app.delete("/api/menus/:id", (req, res) => {
 // ============================
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
-  console.log(`🗄️  DB_HOST: ${process.env.DB_HOST}`);
-  console.log(`🗄️  DB_NAME: ${process.env.DB_NAME}`);
-  console.log(`🗄️  DB_PORT: ${process.env.DB_PORT}`);
+  console.log(`🗄️  DB_HOST: ${process.env.MYSQLHOST}`);
+  console.log(`🗄️  DB_NAME: ${process.env.MYSQLDATABASE}`);
+  console.log(`🗄️  DB_PORT: ${process.env.MYSQLPORT}`);
 });
